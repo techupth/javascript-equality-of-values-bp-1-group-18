e6f86200f3ea0ae343595408ad31974ab4a05d7d
bodin <bodinsonsawan@gmail.com>
2023-10-01 01:23:51 +0700

    edit(ex1-3):edit code for bodin's assignment

--- ex-3.js
+++ ex-3.js
@@ -1,13 +1,12 @@
 function isPlainObject(value) {
-  // Start coding here
+  if (value === null) {
+    return false; 
+  } else if (typeof value === 'object') {
+    return true; 
+  } else if (Array.isArray(value)) {
+    return false; 
+  } else {
+    return "This is not an object"; 
+  }
 }
-
-// Example case
-let result1 = isPlainObject({ name: "John" });
-let result2 = isPlainObject(null);
-let result3 = isPlainObject(["apples", "oranges"]);
-
-// ผลลัพธ์ควรที่ได้จาก Example case
-console.log(result1); //true
-console.log(result2); // false
-console.log(result3); // false
+console.log(isPlainObject(null));
