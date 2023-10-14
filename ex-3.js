6046856ec00cbc499df17633ba6cde92bf9d65db
bread <chalida.bread@gmail.com>
2023-10-14 23:21:37 +0700

    fix: fix code and comment

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,26 @@
 function isPlainObject(value) {
-  // Start coding here
+  const isNull = value === null
+  const isObject = typeof value === "object" && !isNull;
+  const isArray = Array.isArray(value);
+  
+  if (isObject && !isArray) {
+    return true;
+  }
+  if (isArray || isNull) {
+    return false;
+  }
+  return "This is not an object";
 }
+
+const dataType1 = isPlainObject({ type: `object` });
+const dataType2 = isPlainObject([1, 2, 3]);
+const dataType3 = isPlainObject(null);
+const dataType4 = isPlainObject("object");
+
+console.log(dataType1); // true
+console.log(dataType2); // false
+console.log(dataType3); // false
+console.log(dataType4); // This is not an object
 
 // Example case
 let result1 = isPlainObject({ name: "John" });
